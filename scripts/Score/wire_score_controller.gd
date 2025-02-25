194cbff3759a7ec9ba06e0ed703660c56dbccee3
Marcos Vinícius Marcon <marconvcm@gmail.com>
2025-02-25 09:27:20 +0100

    add missing files

---
+++ scripts/Score/wire_score_controller.gd
@@ -0,0 +1,18 @@
+class_name WireScoreController extends Node
+
+@export var template: String = "Stims: %s"
+@export var left_pad: int = 3
+@export var left_pad_char: String = "0"
+@export var label: Label
+@export var score_type: Score.ScoreType
+
+func _ready():
+	if label == null:
+		label = get_parent()
+	if label == null:
+		push_error("WireScoreController requires a Label node to display the score.")
+		return
+
+func _process(_delta):
+	if label != null:
+		label.text = template % str(Score.get_score(score_type)).lpad(left_pad, "0")
