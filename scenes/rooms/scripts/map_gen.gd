db28cd293e866e436c9df13ef922d2d5769d4efc
e3c <e3cunity@gmail.com>
2025-02-26 17:39:43 -0600

    refactor 1

--- scenes/rooms/scripts/map_gen.gd
+++ scenes/rooms/scripts/map_gen.gd
@@ -9,13 +9,14 @@ const ROOM_TYPES := {
 	"cafeteria": {"scene": preload("res://scenes/rooms/cafeteria_a.tscn"), "size": Vector2(60, 30)}
 }
 
-@export var class_room_num := 8
+@export var class_room_num := 6
 var rooms := []
 @export var tile_size := 16
 @export var scale_factor := 4
 var spacing := tile_size * scale_factor
 var previous_direction := Vector2.RIGHT  # store previous direction
-var corridor_tile := Vector2i(9, 3)  # floor
+@export var corridor_tile := Vector2i(9, 3)  # floor
+@export var wall_tile := Vector2i(2, 4)  # floor
 const CORRIDOR_WIDTH := 6  # 6 tiles wide
 var corner_width = 6
 var corner_height = 6
@@ -33,17 +34,17 @@ func generate_aligned_rooms():
 	
 	var main_hall_rect := Rect2(current_pos, main_hall_size)
 	rooms.append(main_hall_rect)
-	spawn_room_instance(main_hall_rect, main_hall_data["scene"], main_hall_data)
+	spawn_room_instance(main_hall_rect, main_hall_data["scene"])
 	
 	var gym_data := ROOM_TYPES["gym"]
 	var gym_size = gym_data["size"] * spacing
 	var gym_rect = main_hall_rect
-	var gym_pos = find_valid_position(gym_rect, gym_size, gym_data)
+	var gym_pos = find_valid_position(gym_rect, gym_size)
 	gym_rect = Rect2(gym_pos, gym_size)
 	
 	if gym_pos != Vector2.INF: # If a valid
 		rooms.append(gym_rect)
-		spawn_room_instance(gym_rect, gym_data["scene"], gym_data)
+		spawn_room_instance(gym_rect, gym_data["scene"])
 		gym_rect.position = gym_pos
 		#print("Gym placed at:", gym_pos)
 	else:
@@ -52,12 +53,12 @@ func generate_aligned_rooms():
 	var cafe_data := ROOM_TYPES["cafeteria"]
 	var cafe_size = cafe_data["size"] * spacing
 	var cafe_rect := main_hall_rect
-	var cafe_pos = find_valid_position(cafe_rect, cafe_size, cafe_data)
+	var cafe_pos = find_valid_position(cafe_rect, cafe_size)
 	cafe_rect = Rect2(cafe_pos, cafe_size)
 	
 	if cafe_pos != Vector2.INF:
 		rooms.append(cafe_rect)
-		spawn_room_instance(cafe_rect, cafe_data["scene"], cafe_data)
+		spawn_room_instance(cafe_rect, cafe_data["scene"])
 		cafe_rect.position = cafe_pos
 		#print("cafe placed at:", cafe_pos)
 	else:
@@ -68,19 +69,19 @@ func generate_aligned_rooms():
 		var class_room_data = ROOM_TYPES["class_room"]
 		var class_room_size = class_room_data["size"] * spacing
 		var random_room = possible_room_positions[randi() % possible_room_positions.size()]
-		var class_room_pos = find_valid_position(random_room, class_room_size, class_room_data)
+		var class_room_pos = find_valid_position(random_room, class_room_size)
 		
 		if class_room_pos != Vector2.INF:
 			var class_room_rect = Rect2(class_room_pos, class_room_size)
 			rooms.append(class_room_rect)
-			spawn_room_instance(class_room_rect, class_room_data["scene"], class_room_data)
+			spawn_room_instance(class_room_rect, class_room_data["scene"])
 			possible_room_positions.clear()
 			possible_room_positions.append(class_room_rect)
 			#print("class room placed at:", class_room_pos)
 		else:
 			print("Failed to place class room")
 
-func find_valid_position(base_room: Rect2, room_size: Vector2, base_room_data: Dictionary) -> Vector2:
+func find_valid_position(base_room: Rect2, room_size: Vector2) -> Vector2:
 	var directions = [
 		Vector2.RIGHT, 
 		Vector2.LEFT, 
@@ -105,7 +106,7 @@ func is_room_valid(room: Rect2) -> bool:
 			return false  
 	return true
 
-func spawn_room_instance(room: Rect2, scene: PackedScene, room_type: Dictionary):
+func spawn_room_instance(room: Rect2, scene: PackedScene):
 	var room_instance = scene.instantiate()
 	room_instance.global_position = room.position
 	#room_instance.rotation = rotation  # rotation
@@ -147,13 +148,13 @@ func add_corridors():
 
 		# corridors
 		if not adjacent_rooms["left"]:
-			extend_border(tilemap, room_rect, "left")
+			extend_border(tilemap, room_rect, "left", room.name)
 		if not adjacent_rooms["right"]:
-			extend_border(tilemap, room_rect, "right")
+			extend_border(tilemap, room_rect, "right" , room.name)
 		if not adjacent_rooms["top"]:
-			extend_border(tilemap, room_rect, "top")
+			extend_border(tilemap, room_rect, "top" , room.name)
 		if not adjacent_rooms["bottom"]:
-			extend_border(tilemap, room_rect, "bottom")
+			extend_border(tilemap, room_rect, "bottom" , room.name)
 		# corners
 		if not adjacent_rooms["left"] and not adjacent_rooms["top"]:
 			extend_corner(tilemap, room_rect, "top_left")
@@ -180,10 +181,10 @@ func extend_corner(tilemap: TileMapLayer, room_rect: Rect2, corner_type: String)
 		var corner_start = Vector2i(top_left_coord.position.x - 1, top_left_coord.position.y - 1)
 		for x in range(corner_width + 1):
 			var tile_position = Vector2i(corner_start.x + x, corner_start.y)
-			tilemap.set_cell(tile_position, 0, Vector2i(2,4))
+			tilemap.set_cell(tile_position, 0, wall_tile)
 		for y in range(corner_height + 1):
 			var tile_position = Vector2i(corner_start.x, corner_start.y + y)
-			tilemap.set_cell(tile_position, 0, Vector2i(2,4))
+			tilemap.set_cell(tile_position, 0, wall_tile)
 
 	elif corner_type == "top_right":
 		#print("Corner: ", corner_type, " position: ", top_right_coord)
@@ -194,10 +195,10 @@ func extend_corner(tilemap: TileMapLayer, room_rect: Rect2, corner_type: String)
 		var corner_start = Vector2i(top_right_coord.position.x, top_right_coord.position.y - 1)
 		for x in range(corner_width + 1):
 			var tile_position = Vector2i(corner_start.x + x, corner_start.y)
-			tilemap.set_cell(tile_position, 0, Vector2i(2,4))
+			tilemap.set_cell(tile_position, 0, wall_tile)
 		for y in range(corner_height + 1):
 			var tile_position = Vector2i(corner_start.x + top_right_coord.size.x, corner_start.y + y)
-			tilemap.set_cell(tile_position, 0, Vector2i(2,4))
+			tilemap.set_cell(tile_position, 0, wall_tile)
 
 	elif corner_type == "bottom_left":
 		#print("Corner: ", corner_type, " position: ", bottom_left_coord)
@@ -208,10 +209,10 @@ func extend_corner(tilemap: TileMapLayer, room_rect: Rect2, corner_type: String)
 		var corner_start = Vector2i(bottom_left_coord.position.x - 1, bottom_left_coord.position.y - 1)
 		for x in range(corner_width + 1):
 			var tile_position = Vector2i(corner_start.x + x, corner_start.y + bottom_left_coord.size.y + 1)
-			tilemap.set_cell(tile_position, 0, Vector2i(2,4))
+			tilemap.set_cell(tile_position, 0, wall_tile)
 		for y in range(corner_height + 1):
 			var tile_position = Vector2i(corner_start.x, corner_start.y + y)
-			tilemap.set_cell(tile_position, 0, Vector2i(2,4))
+			tilemap.set_cell(tile_position, 0, wall_tile)
 
 	elif corner_type == "bottom_right":
 		#print("Corner: ", corner_type, " position: ", bottom_right_coord)
@@ -222,10 +223,10 @@ func extend_corner(tilemap: TileMapLayer, room_rect: Rect2, corner_type: String)
 		var corner_start = Vector2i(bottom_right_coord.position.x - 1, bottom_right_coord.position.y - 1)
 		for x in range(corner_width + 2):
 			var tile_position = Vector2i(corner_start.x + x, corner_start.y + bottom_right_coord.size.y + 1)
-			tilemap.set_cell(tile_position, 0, Vector2i(2,4))
+			tilemap.set_cell(tile_position, 0, wall_tile)
 		for y in range(corner_height + 1):
 			var tile_position = Vector2i(corner_start.x + bottom_right_coord.size.x + 1, corner_start.y + y)
-			tilemap.set_cell(tile_position, 0, Vector2i(2,4))
+			tilemap.set_cell(tile_position, 0, wall_tile)
 
 func check_adjacent(room_rect: Rect2, dir: Vector2) -> bool:
 	var check_position = room_rect.position + (dir * Vector2(room_rect.size.x + spacing, room_rect.size.y + spacing))
@@ -236,7 +237,7 @@ func check_adjacent(room_rect: Rect2, dir: Vector2) -> bool:
 			return true
 	return false
 
-func extend_border(tilemap, room_rect: Rect2, direction: String):
+func extend_border(tilemap, room_rect: Rect2, direction: String, name: String):
 	var room_start = tilemap.local_to_map(Vector2i(0, 0))
 	var room_end = tilemap.local_to_map(Vector2i(room_rect.size.x / scale_factor, room_rect.size.y / scale_factor))
 	
@@ -245,68 +246,99 @@ func extend_border(tilemap, room_rect: Rect2, direction: String):
 	#print("DIAGONALS: ", diagonals)
 	
 	if direction == "left":
-		print("DIA: ", diagonals)
-		var topleftstart = 0
-		var bottomleftstart = 0
-		
-		if Vector2(-1, -1) in diagonals:
-			topleftstart = 6
-					
-		if Vector2(-1, 1) in diagonals:
-			bottomleftstart = -6
-
+		var toprightstart = 0
+		var bottomrightstart = 0
 		for y in range(room_start.y, room_end.y + 1):
 			for x_offset in range(-CORRIDOR_WIDTH, 0):
-				if x_offset == -6:
-					tilemap.set_cell(Vector2i(room_start.x + x_offset - 1, y + topleftstart + bottomleftstart), 0, Vector2i(2,4))
-				tilemap.set_cell(Vector2i(room_start.x + x_offset, y), 0, corridor_tile)
 				
+				# do walls
+				if Vector2(-1, -1) in diagonals: # left side
+					toprightstart = corner_width
+					#print(name, " left left side ", y + toprightstart)
+					if x_offset == -CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(room_start.x + x_offset - 1, y + toprightstart), 0, wall_tile)
+				elif Vector2(-1, 1) in diagonals: # right side
+					bottomrightstart = corner_width
+					#print(name, " left right side ", y - bottomrightstart)
+					if x_offset == -CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(room_start.x + x_offset - 1, y - bottomrightstart), 0, wall_tile)
+				else:
+					#print(name, " left full ", y)
+					if x_offset == -CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(room_start.x + x_offset - 1, y), 0, wall_tile)
+				# do floors
+				tilemap.set_cell(Vector2i(room_start.x + x_offset, y), 0, corridor_tile)
+
 	elif direction == "right":
 		var toprightstart = 0
 		var bottomrightstart = 0
-		
-		if Vector2(1, -1) in diagonals:
-			toprightstart = 6
-					
-		if Vector2(1, 1) in diagonals:
-			bottomrightstart = -6
-						
 		for y in range(room_start.y, room_end.y + 1):
 			for x_offset in range(1, CORRIDOR_WIDTH + 1):
-				if x_offset == 6:
-					tilemap.set_cell(Vector2i(room_end.x + x_offset + 1, y + toprightstart + bottomrightstart), 0, Vector2i(2,4))
-				tilemap.set_cell(Vector2i(room_end.x + x_offset, y), 0, corridor_tile)
 				
+				# do walls
+				if Vector2(1, -1) in diagonals: # left side
+					toprightstart = corner_width
+					#print(name, " right left side ", y + toprightstart)
+					if x_offset == CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(room_end.x + x_offset + 1, y + toprightstart), 0, wall_tile)
+				elif Vector2(1, 1) in diagonals: # right side
+					bottomrightstart = corner_width
+					#print(name, " right right side ", y - bottomrightstart)
+					if x_offset == CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(room_end.x + x_offset + 1, y - bottomrightstart), 0, wall_tile)
+				else:
+					#print(name, " right full ", y)
+					if x_offset == CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(room_end.x + x_offset + 1, y), 0, wall_tile)
+				# do floors
+				tilemap.set_cell(Vector2i(room_end.x + x_offset, y), 0, corridor_tile)
+
 	elif direction == "top":
 		var leftstart = 0
 		var rightstart = 0
-		
-		if Vector2(-1, -1) in diagonals:
-			leftstart = 6
-					
-		if Vector2(1, -1) in diagonals:
-			rightstart = -6
-			
 		for x in range(room_start.x, room_end.x + 1):
 			for y_offset in range(-CORRIDOR_WIDTH, 0):
-				if y_offset == -6:
-					tilemap.set_cell(Vector2i(x + leftstart + rightstart, room_start.y + y_offset - 1), 0, Vector2i(2,4))
-				tilemap.set_cell(Vector2i(x, room_start.y + y_offset), 0, corridor_tile)
 				
+				# do walls
+				if Vector2(-1, -1) in diagonals: # left side
+					leftstart = corner_width
+					#print(name, " top left side ", x + leftstart)
+					if y_offset == -CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(x + leftstart, room_start.y + y_offset - 1), 0, wall_tile)
+				elif Vector2(1, -1) in diagonals: # right side
+					rightstart = corner_width
+					#print(name, " top right side ", x - rightstart)
+					if y_offset == -CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(x - rightstart, room_start.y + y_offset - 1), 0, wall_tile)
+				else:
+					#print(name, " top full ", x)
+					if y_offset == -CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(x, room_start.y + y_offset - 1), 0, wall_tile)
+				# do floors
+				tilemap.set_cell(Vector2i(x, room_start.y + y_offset), 0, corridor_tile)
+
 	elif direction == "bottom":
 		var leftstart = 0
 		var rightstart = 0
-		
-		if Vector2(-1, 1) in diagonals:
-			leftstart = 6
-					
-		if Vector2(1, 1) in diagonals:
-			rightstart = -6
-			
 		for x in range(room_start.x, room_end.x + 1):
 			for y_offset in range(1, CORRIDOR_WIDTH + 1):
-				if y_offset == 6:
-					tilemap.set_cell(Vector2i(x + leftstart + rightstart, room_end.y + y_offset + 1), 0, Vector2i(2,4))
+				
+				# do walls
+				if Vector2(-1, 1) in diagonals: # left side
+					leftstart = corner_width
+					#print(name, " bottom left side ", x + leftstart)
+					if y_offset == CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(x + leftstart, room_end.y + y_offset + 1), 0, wall_tile)
+				elif Vector2(1, 1) in diagonals: # right side
+					rightstart = corner_width
+					#print(name, " bottom right side ", x - rightstart)
+					if y_offset == CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(x - rightstart, room_end.y + y_offset + 1), 0, wall_tile)
+				else:
+					#print(name, " bottom full ", x)
+					if y_offset == CORRIDOR_WIDTH:
+						tilemap.set_cell(Vector2i(x, room_end.y + y_offset + 1), 0, wall_tile)
+				# do floors
 				tilemap.set_cell(Vector2i(x, room_end.y + y_offset), 0, corridor_tile)
 	
 	#print("Room World Pos: ", room_rect.position)
