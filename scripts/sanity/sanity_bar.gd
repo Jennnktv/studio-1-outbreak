b98cb9385271cfac14adcf83fc915a2c1cf3eddb
Bristlethorn <jmolloaustin@gmail.com>
2025-03-05 07:18:40 -0500

    Sanity Bar Animation
    - Added AnimatedSprite2D sprite frames to the Sanity Bar, animated through code.

--- scripts/sanity/sanity_bar.gd
+++ scripts/sanity/sanity_bar.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var sanity_bar: ProgressBar = $MarginContainer/VBoxContainer/ProgressBar
 @onready var fury_time: Timer = $FuryTime
+@onready var eye_sprite: AnimatedSprite2D = $MarginContainer/AnimatedSprite2D
+
 var sanity:float
 var sanity_loss = 0.05
 
@@ -16,15 +18,33 @@ func _process(_delta: float) -> void:
 	sanity = sanity_bar.value
 	
 	SignalBus.current_stim.emit(sanity)
-	if sanity <= 50:
-		is_withdrawal()
-	
 	if sanity < 1:
 		is_dead()
 	
+	if sanity >= 1 and sanity < 10:
+		eye_sprite.frame = 0
+	
+	if sanity >= 10 and sanity < 20:
+		eye_sprite.frame = 1
+		
+	if sanity >= 20 and sanity < 40:
+		eye_sprite.frame = 2
+	
+	if sanity >= 40 and sanity < 60:
+		eye_sprite.frame = 3
+		
+	if sanity <= 50:
+		is_withdrawal()
+	
+	if sanity >= 60 and sanity < 80:
+		eye_sprite.frame = 4
+	
+	if sanity >= 80 and sanity <= 99:
+		eye_sprite.frame = 5
+	
 	if sanity > 99:
 		is_fury()
-		
+		eye_sprite.frame = 6
 
 
 func is_withdrawal() -> void:
@@ -42,7 +62,7 @@ func is_fury() -> void:
 
 
 func on_stim_collected() -> void:
-	sanity_bar.value += 5
+	sanity_bar.value += 20
 
 
 func _on_sanity_loss_timeout() -> void:
